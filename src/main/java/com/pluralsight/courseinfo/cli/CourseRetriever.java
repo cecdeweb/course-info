781c27dc15318977bfa2b691179ce90746a2e1e3
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2025-05-09 14:05:48 +0200

    Initial commit: Project setup, CLI and SLF4J integration

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,34 @@
+package com.pluralsight.courseinfo.cli;
+
+import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String... args){
+
+        LOG.info("CourseRetriever starting");
+        if(args.length == 0){
+            LOG.warn("Merci d ajouter un nom d auteur comme premier argument");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e){
+            LOG.error("Unexpected error", e);
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+        LOG.info("Obtenir des cours pour l auteur '{}'", authorId);
+        CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
+
+        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
+        LOG.info("Retrieved the following courses {}", coursesToStore);
+
+    }
+}
